8e11df60cc8480c819a61a4f638f1555cdcdaab0
J-M <per202@csiro.au>
2020-01-22 15:42:29 +1100

    Pick up LD_LIBRARY_PATH environment variable from the R startup script
    The usual R startup script changes the environment variable LD_LIBRARY_PATH before starting the R native executable. For some setups e.g. /usr/local/lib/R/bin/exec/R installed from a source tarball, you may end up with "error while loading shared libraries: libRblas.so: cannot open shared object file: No such file or directory" .

--- .vscode/debug.R
+++ .vscode/debug.R
@@ -1,4 +1,5 @@
 env <- Sys.getenv()
 names <- names(env)
-rnames <- names[startsWith(names, "R_")]
+rnames <- names[startsWith(names, "LD_LIBRARY")]
+rnames <- c( rnames, names[startsWith(names, "R_")] )
 writeLines(paste0(rnames, "=", env[rnames]), ".vscode/.env")
